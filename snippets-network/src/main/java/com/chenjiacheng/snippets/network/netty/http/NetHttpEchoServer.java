ca78dfe11bf883803681e76bdfc90d6cbfe9b23c
chenjiacheng <chen-jiacheng@foxmail.com>
2024-03-26 00:59:19 +0800

    Add network codes

---
+++ snippets-network/src/main/java/com/chenjiacheng/snippets/network/netty/http/NetHttpEchoServer.java
@@ -0,0 +1,10 @@
+package com.chenjiacheng.snippets.network.netty.http;
+
+/**
+ * Created by chenjiacheng on 2024/3/26 00:58
+ *
+ * @author chenjiacheng
+ * @since 1.0.0
+ */
+public class NetHttpEchoServer {
+}
---
+++ snippets-network/src/main/java/com/chenjiacheng/snippets/network/netty/ws/NettyWebSocketEchoServer.java
@@ -0,0 +1,10 @@
+package com.chenjiacheng.snippets.network.netty.ws;
+
+/**
+ * Created by chenjiacheng on 2024/3/26 00:57
+ *
+ * @author chenjiacheng
+ * @since 1.0.0
+ */
+public class NettyWebSocketEchoServer {
+}
