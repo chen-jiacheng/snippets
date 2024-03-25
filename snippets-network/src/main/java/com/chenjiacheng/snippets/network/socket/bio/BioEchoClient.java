80af0815fbfa2bdfea1c1ec4e2e4cc6fb5a7374c
chenjiacheng <chen-jiacheng@foxmail.com>
2024-03-26 00:51:12 +0800

    新增socket相关代码

---
+++ snippets-network/src/main/java/com/chenjiacheng/snippets/network/socket/bio/BioEchoClient.java
@@ -0,0 +1,10 @@
+package com.chenjiacheng.snippets.network.socket.bio;
+
+/**
+ * Created by chenjiacheng on 2024/3/26 00:40
+ *
+ * @author chenjiacheng
+ * @since 1.0.0
+ */
+public class BioEchoClient {
+}
---
+++ snippets-network/src/main/java/com/chenjiacheng/snippets/network/socket/nio/reactor/ReactorEchoClient.java
@@ -0,0 +1,10 @@
+package com.chenjiacheng.snippets.network.socket.nio.reactor;
+
+/**
+ * Created by chenjiacheng on 2024/3/26 00:45
+ *
+ * @author chenjiacheng
+ * @since 1.0.0
+ */
+public class ReactorEchoClient {
+}
